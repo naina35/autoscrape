726c077a5c35ec485acd5e7748b76428bb123eb9
Prabhpreet <kprabhpreet25@gmail.com>
2025-07-16 17:50:41 +0530

    razorpay logic change- 1

--- lib/razorpay/razorpay.ts
+++ lib/razorpay/razorpay.ts
@@ -1,5 +1,5 @@
 const Razorpay = require('razorpay')
 
- const razorpay = new Razorpay({key_id: process.env.RAZORPAY_KEY_ID!,   key_secret: process.env.RAZORPAY_KEY_SECRET!})
+ const razorpay = new Razorpay({key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,   key_secret: process.env.RAZORPAY_KEY_SECRET!})
 
  export default razorpay;
