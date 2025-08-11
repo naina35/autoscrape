150fc822953dc052d15fac3112441118207b4357
Prabh Khosa <kprabhpreet25@gmail.com>
2025-08-11 23:22:32 +0530

    Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.js
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
