204c0a161f878b52d4869f0fcb2def3fe0f4e67f
Prabh Khosa <kprabhpreet25@gmail.com>
2025-08-11 23:16:53 +0530

    pupeteer

--- lib/pupeteer.ts
+++ lib/pupeteer.ts
@@ -1,13 +1,17 @@
-import puppeteer from "puppeteer-core";
-import chrome from "chrome-aws-lambda";
+// lib/puppeteer.ts
+import puppeteer, { Browser, Page } from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
-export async function startBrowser() {
-  const executablePath = await chrome.executablePath;
+export async function startBrowser(): Promise<{ browser: Browser; page: Page }> {
+  const executablePath =
+    process.env.AWS_EXECUTION_ENV // Detect if running on AWS Lambda/Vercel
+      ? await chromium.executablePath
+      : "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"; // Local dev path
 
   const browser = await puppeteer.launch({
-    args: chrome.args,
+    headless: true,
     executablePath: executablePath || undefined,
-    headless: chrome.headless,
+    args: chromium.args,
   });
 
   const page = await browser.newPage();
