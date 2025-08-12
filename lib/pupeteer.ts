536039941a52bda477e02830e45655f7583f6f08
Prabh Khosa <kprabhpreet25@gmail.com>
2025-08-13 00:06:23 +0530

    serverless

--- lib/pupeteer.ts
+++ lib/pupeteer.ts
@@ -5,10 +5,8 @@ import chromium from "@sparticuz/chromium";
 export async function startBrowser(): Promise<{ browser: Browser; page: Page }> {
   const isServerless = !!process.env.AWS_EXECUTION_ENV; // Detect if running in AWS Lambda / Vercel
 
-  const executablePath = isServerless
-    ? await chromium.executablePath() // Sparticuz's method returns Promise<string>
-    : "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"; // Local dev path
-
+  const executablePath = await chromium.executablePath() // Sparticuz's method returns Promise<string>
+//"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: executablePath || undefined,
